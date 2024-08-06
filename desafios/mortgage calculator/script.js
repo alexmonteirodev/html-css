26b1d14b9182d09c8db68351a20fae5fd0f82e7e
alexmonteirodev <159573548+alexmonteirodev@users.noreply.github.com>
2024-08-06 13:45:08 +0200

    input checkbox

--- desafios/mortgage calculator/script.js
+++ desafios/mortgage calculator/script.js
@@ -5,11 +5,25 @@ var interestrateinput = document.getElementById('interestrateinput')
 var radioinput = document.getElementsByClassName('radioinput')
 
 clearbutton.addEventListener('click', clear)
-function clear(params) {
+function clear(params) { //clear button
     mortgageinput.value = ''
     mortgageterminput.value = ''
     interestrateinput.value = ''
     for (let i = 0; i < radioinput.length; i++) {
         radioinput[i].checked = false
     }
+}
+
+for (let i = 0; i < radioinput.length; i++) {
+        radioinput[i].addEventListener('click',radioinpt)
+
+
+// radioinput.addEventListener('click',radioinpt)
+function radioinpt(params) {
+    if (radioinput[i].checked) {
+        window.alert('deu bom')
+    } else {
+        window.alert('deu ruim')
+    }
+}
 }
